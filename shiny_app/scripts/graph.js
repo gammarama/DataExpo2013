ce760a37faa59a38390469adce67d60407f81679
Andee Kaplan <andee.kaplan@gmail.com>
2013-08-11 11:53:36 -0500

    added google analytics

--- shiny_app/scripts/graph.js
+++ shiny_app/scripts/graph.js
@@ -5,6 +5,19 @@
 <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 <script type="text/javascript">
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38420239-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<script type="text/javascript">
+
 var return_value = 0;
 
 var outputBinding = new Shiny.OutputBinding();
